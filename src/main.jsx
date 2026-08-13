8eb11e6886418804c2295cfa5484fced2b18aefb
Rashidbek <rasishidbekrasuljonov83>
2026-08-13 14:08:29 +0300

    vazifa

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-
+import "./index.css";
 import App from "./App.jsx";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
